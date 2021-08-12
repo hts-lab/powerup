844363bc23f05b521290f81671fd7677c7bea023
Mushriq <mushriq@live.com>
2021-08-12 13:17:23 +0000

    Added get_legend and hues

--- R/misc.R
+++ R/misc.R
@@ -13,3 +13,40 @@ show_msg <- function(x){
     
 }
 
+
+
+
+#' Extract ggplot legend
+#'
+#' This function extracts the ggplot legend to plot separately.
+#' @param ggplot_object A ggplot object.
+#' @keywords ggplot legend
+#' @export
+#' @examples
+#' get_legend(my_ggplot)
+get_legend <- function(ggplot_object){
+    
+   tmp <- ggplot_gtable(ggplot_build(ggplot_object))
+   legend_id <- which(sapply(tmp$grobs, function(x) x$name) == "guide-box")
+   legend <- tmp$grobs[[legend_id]]
+   return(legend)
+                      
+}
+  
+
+#' Get a palette of specified size
+#'
+#' This function creates a palette of specified size using equal distant hues.
+#' @param n The size of the palette.
+#' @keywords hues
+#' @export
+#' @examples
+#' gg_color_hue(5)                           
+gg_color_hue <- function(n) {
+  hues = seq(15, 375, length = n + 1)
+  hcl(h = hues, l = 65, c = 100)[1:n]
+}
+  
+
+
+
